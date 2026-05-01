fa2cb7051b5a04263a333516a45e9e0c66f71196
Your Name <you@example.com>
2026-05-01 15:45:20 +0200

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,26 @@
+package org.example.project
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Hello World!")
+        }
+    }
+}
